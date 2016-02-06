43c18a0219841c006868dec05c4f7163980fe3cf
nikhileshvyas <nikhileshvyas4455@gmail.com>
2016-02-06 14:40:02 +0530

    fdhdfhdfhdf

--- app/Controller/AccountsController.php
+++ app/Controller/AccountsController.php
@@ -458,7 +458,7 @@ $amount_vv = 1;
 	
 	
 	foreach($v_result as $data){
-		if(array_sum($data)==0 && $tt_v == 0 && $trajection_date_v == 0,$trr_v == 0){ echo "T";
+		if(array_sum($data)==0 && $tt_v == 0 && $trajection_date_v == 0 && $trr_v == 0){ echo "T";
 			$this->loadmodel('import_ob_record');
 			$this->import_ob_record->updateAll(array("step4" => 1),array("society_id" => $s_society_id, "module_name" => "OB"));	
 		}else{ echo "F"; die; }
