b266d06a9c75639480cfb1caf4ff1a65b6e260f3
nikhileshvyas <nikhileshvyas4455@gmail.com>
2016-02-13 11:21:01 +0530

    fjgfjgjgfjf

--- app/Controller/ExpensetrackersController.php
+++ app/Controller/ExpensetrackersController.php
@@ -797,8 +797,146 @@ $this->layout='session';
 $this->ath();
 $this->check_user_privilages();
 $s_society_id=(int)$this->Session->read('society_id');	
-	
+
+
+
+
+$this->loadmodel('import_expense_tracker_record');
+	$conditions=array("society_id" => $s_society_id,"module_name" => "ET");
+	$result_import_record = $this->import_expense_tracker_record->find('all',array('conditions'=>$conditions));
+	$this->set('result_import_record',$result_import_record);
+	foreach($result_import_record as $data_import){
+		$step1=(int)@$data_import["import_expense_tracker_record"]["step1"];
+		$step2=(int)@$data_import["import_expense_tracker_record"]["step2"];
+		$step3=(int)@$data_import["import_expense_tracker_record"]["step3"];
+		$step4=(int)@$data_import["import_expense_tracker_record"]["step4"];
+	}
+
 }
 ////////////////// End expense_tracker_import ///////////////////////////////////////
+////////////////// Start upload_expense_tracker_csv_file //////////////////////
+function upload_expense_tracker_csv_file()
+{
+$s_society_id = $this->Session->read('society_id');
+	$s_user_id=$this->Session->read('user_id');
+	$this->ath();
+	if(isset($_FILES['file'])){
+		$file_name=$s_society_id.".csv";
+		$file_tmp_name =$_FILES['file']['tmp_name'];
+		$target = "expense_tracker_csv_file/";
+		$target=@$target.basename($file_name);
+		move_uploaded_file($file_tmp_name,@$target);
+		
+		
+		$today = date("d-M-Y");
+		
+		$this->loadmodel('import_expense_tracker_record');
+		$auto_id=$this->autoincrement('import_expense_tracker_record','auto_id');
+		$this->import_expense_tracker_record->saveAll(Array( Array("auto_id" => $auto_id, "file_name" => $file_name,"society_id" => $s_society_id, "user_id" => $s_user_id, "module_name" => "ET","step1" => 1,"date"=>$today))); 
+		
+		die(json_encode("UPLOADED"));
+	}	
+}
+//////////////////////// End upload_expense_tracker_csv_file //////////////////
+//////////////////////// Start read_csv_file_expense //////////////////////////
+function read_csv_file_expense()
+{
+$this->layout=null;
+	$s_society_id = $this->Session->read('society_id');
+	
+	$f = fopen('expense_tracker_csv_file/'.$s_society_id.'.csv', 'r') or die("ERROR OPENING DATA");
+	$batchcount=0;
+	$records=0;
+	while (($line = fgetcsv($f, 4096, ';')) !== false) {
+	$numcols = count($line);
+	$test[]=$line;
+	++$records;
+	}
+	$i=0;
+	foreach($test as $child){ $i++;
+		if($i>1){
+			$child_ar=explode(',',$child[0]);
+			$posting_date=@$child_ar[0];
+			$invoice_date=@$child_ar[1];
+			$due_date=@$child_ar[2];
+			$party_ac=@$child_ar[3];
+			$invoice_ref=@$child_ar[4];
+			$expense_head=@$child_ar[5];
+			$amount=@$child_ar[6];
+			$description = $child[7];
+			
+			$this->loadmodel('expense_tracker_csv');
+			$auto_id=$this->autoincrement('expense_tracker_csv','auto_id');
+			$this->expense_tracker_csv->saveAll(Array(Array("auto_id" => $auto_id, "posting_date" => $posting_date,"invoice_date"=>$invoice_date,"due_date"=>$due_date, "party_ac" => $party_ac, "invoice_ref" => $invoice_ref,"expense_head"=>$expense_head,"amount"=>$amount,"description"=>$description,"society_id"=>$s_society_id,"is_converted"=>"NO")));
+		
+	}
+	}
+	$this->loadmodel('import_expense_tracker_record');
+	$this->import_expense_tracker_record->updateAll(array("step2" => 1),array("society_id" => $s_society_id, "module_name" => "ET"));
+	die(json_encode("READ"));	
+	
+	
+}
+/////////////////////// End read_csv_file_expense /////////////////////////////
+////////////////////// Start convert_imported_data_et //////////////////////////////
+function convert_imported_data_et()
+{
+$this->layout=null;
+	$s_society_id = $this->Session->read('society_id');
+	
+	$this->loadmodel('opening_balance_csv');
+	$conditions=array("society_id" => $s_society_id,"is_converted" => "NO");
+	$result_import_record = $this->opening_balance_csv->find('all',array('conditions'=>$conditions,'limit'=>20));
+	foreach($result_import_record as $import_record){
+
+$ob_id=(int)@$import_record["opening_balance_csv"]["auto_id"];
+$group_name=trim(@$import_record["opening_balance_csv"]["group"]);
+$account_name=trim(@$import_record["opening_balance_csv"]["subledger_ac"]);
+$wing_name=@$import_record["opening_balance_csv"]["wing_name"];
+$flat_name=(int)@$import_record["opening_balance_csv"]["flat_name"];
+$type=@$import_record["opening_balance_csv"]["type"];
+$amount=@$import_record["opening_balance_csv"]["amount"];
+$penalty=@$import_record["opening_balance_csv"]["penalty"];
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////		
+
+		$this->loadmodel('opening_balance_csv_converted');
+		$auto_iddd=$this->autoincrement('opening_balance_csv_converted','auto_id');
+		$this->opening_balance_csv_converted->saveAll(Array(Array("auto_id" => $auto_iddd, "group_id"=>$group_id,"ledger_id" => $auto_id,"ledger_type" => $ledger_type, "wing_id" => $wing_id, "flat_id" => $flat_id,"type"=>$type_id,"amount"=>$amount,"penalty"=>$penalty,"society_id"=>$s_society_id,"is_imported"=>"NO")));
+		
+		$this->loadmodel('opening_balance_csv');
+		$this->opening_balance_csv->updateAll(array("is_converted" => "YES"),array("auto_id" => $ob_id));
+	}
+	
+	$this->loadmodel('opening_balance_csv');
+	$conditions=array("society_id" => $s_society_id,"is_converted" => "YES");
+	$total_converted_records = $this->opening_balance_csv->find('count',array('conditions'=>$conditions));
+	
+	$this->loadmodel('opening_balance_csv');
+	$conditions=array("society_id" => $s_society_id);
+	$total_records = $this->opening_balance_csv->find('count',array('conditions'=>$conditions));
+	
+	$converted_per=($total_converted_records*100)/$total_records;
+	if($converted_per==100){ $again_call_ajax="NO"; 
+		$this->loadmodel('import_ob_record');
+		$this->import_ob_record->updateAll(array("step3" => 1),array("society_id" => $s_society_id, "module_name" => "OB"));
+	}else{
+		$again_call_ajax="YES"; 
+			
+		}
+	die(json_encode(array("again_call_ajax"=>$again_call_ajax,"converted_per"=>$converted_per)));	
+		
+	
+}
+///////////////// End convert_imported_data_et /////////////////////////////////////
 }
 ?>
